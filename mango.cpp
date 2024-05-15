3ecdb1df0d23744b1e8741892fcb177159cb203d
Badhan Paul <86283575+Badhan0@users.noreply.github.com>
2024-05-15 14:52:47 +0530

    Update mango.cpp

--- mango.cpp
+++ mango.cpp
@@ -1,53 +1,24 @@
 #include <iostream>
-using namespace std; 
-#define NA -1 
-void moveToEnd(int mPlusN[], int size) 
-{ 
-int j = size - 1; 
-for (int i = size - 1; i >= 0; i--) 
-	if (mPlusN[i] != NA) 
-	{ 
-		mPlusN[j] = mPlusN[i]; 
-		j--; 
-	} 
-} 
-void merge(int mPlusN[], int N[], int m, int n) 
-{ 
-int i = n;
-int j = 0; 
-int k = 0; 
-while (k < (m + n)) 
-{ 
-	if ((j == n)||(i > (m + n) && mPlusN[i] <= N[j])) 
-	{ 
-		mPlusN[k] = mPlusN[i]; 
-		k++; 
-		i++; 
-	} 
-	else
-	{ 
-	mPlusN[k] = N[j]; 
-	k++; 
-	j++; 
-	} 
-} 
-} 
-void printArray(int arr[], int size) 
-{ 
-for (int i = 0; i < size; i++) 
-cout << arr[i] << " "; 
+#include <algorithm>
+using namespace std;
 
-cout << endl; 
-} 
 int main() 
-{ 
-int mPlusN[] = {2, 8, NA, NA, NA, 13, NA, 15, 20}; 
-int N[] = {25, 9, 7, 5}; 
-int n = sizeof(N) / sizeof(N[0]); 
-int m = sizeof(mPlusN) / sizeof(mPlusN[0]) - n; 
-moveToEnd(mPlusN, m + n); 
-merge(mPlusN, N, m, n); 
-printArray(mPlusN, m+n); 
-
-return 0; 
-} 
+{
+    int A[] = {1, 3, 5, 7}; 
+    int B[] = {8, 6, 4, 2}; 
+    int n = sizeof(B) / sizeof(B[0]); 
+    int m = sizeof(A) / sizeof(A[0]);
+    int a=m+n;
+    int C[a];
+    
+    for (int i = 0, j = 0, k = 0; k < a;) {
+    if (A[i] < B[j])
+      C[k++] = A[i++];
+    else
+      C[k++] = B[j++];
+  }
+  sort(C, C + a);
+  for (int k = 0; k < a; k++) {
+    cout<<C[k];
+  }
+}
